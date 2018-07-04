c6f4cf4f757f75e4c7d99cf466aba0960c4a2a38
shicunyong <18338766465@163.com>
2018-07-04 16:03:20 +0800

    '[add.]'

--- server.js
+++ server.js
@@ -1,20 +1,151 @@
-var http = require("http");
-var url = require("url");
+// var http = require("http");
+// var url = require("url");
+
+// function start(route) {
+//   function onRequest(request, response) {
+//     var pathname = url.parse(request.url).pathname;
+//     console.log("Request for " + pathname + " received.");
+
+//     route(pathname);
+
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+//   }
+
+//   http.createServer(onRequest).listen(8888);
+//   console.log("Server has started.");
+// }
+
+// exports.start = start;
+
+// var express = require('express');
+// var app = express();
+
+// app.use(express.static('public'));
+
+// app.get('/index.html', function (req, res) {
+//   res.sendFile(__dirname + "/" + "index.html");
+// })
+
+// app.get('/process_get', function (req, res) {
+
+//   // 输出 JSON 格式
+//   var response = {
+//     "first_name": req.query.first_name,
+//     "last_name": req.query.last_name
+//   };
+//   console.log(response);
+//   res.end(JSON.stringify(response));
+// })
+
+// var server = app.listen(8081, function () {
+
+//   var host = server.address().address
+//   var port = server.address().port
+
+//   console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+// })
+
+
+// var express = require('express');
+// var app = express();
+// var fs = require("fs");
  
-function start(route) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+// var bodyParser = require('body-parser');
+// var multer  = require('multer');
  
-    route(pathname);
+// app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(multer({ dest: '/tmp/'}).array('image'));
  
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
+// app.get('/index.htm', function (req, res) {
+//    res.sendFile( __dirname + "/" + "index.htm" );
+// })
  
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+// app.post('/file_upload', function (req, res) {
+ 
+//    console.log(req.files[0]);  // 上传的文件信息
+ 
+//    var des_file = __dirname + "/" + req.files[0].originalname;
+//    fs.readFile( req.files[0].path, function (err, data) {
+//         fs.writeFile(des_file, data, function (err) {
+//          if( err ){
+//               console.log( err );
+//          }else{
+//                response = {
+//                    message:'File uploaded successfully', 
+//                    filename:req.files[0].originalname
+//               };
+//           }
+//           console.log( response );
+//           res.end( JSON.stringify( response ) );
+//        });
+//    });
+// })
+ 
+// var server = app.listen(8081, function () {
+ 
+//   var host = server.address().address
+//   var port = server.address().port
+ 
+//   console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+// })
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+//添加的新用户数据
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
 }
- 
-exports.start = start;
+
+// app.get('/:id', function (req, res) {
+//   // 首先我们读取已存在的用户
+//   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+//       data = JSON.parse( data );
+//       var user = data["user" + req.params.id] 
+//       console.log( req.params );
+//       // console.log( user );
+//       res.end( JSON.stringify(user));
+//   });
+// })
+
+app.get('/addUser', function (req, res) {
+   // 读取已存在的数据
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data["user4"] = user["user4"];
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
+var id = 2;
+
+app.get('/deleteUser', function (req, res) {
+
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       delete data["user" + 2];
+       
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
